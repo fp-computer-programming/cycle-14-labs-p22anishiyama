17181005bc0b8e9ed0ba02270dfea6240cb76804
p22anishiyama <p22anishiyama@fairfieldprep.org>
2022-03-17 10:03:25 -0400

    add lab_14-1.py lab_14-2.py

---
+++ lab_14-1.py
@@ -0,0 +1,20 @@
+# Author: ATN 3/17/22
+
+import turtle
+
+window = turtle.Screen()
+window.setup(1000, 1000)
+window.screensize(950, 950)
+
+t = turtle.Turtle()
+
+t.right(90)
+t.forward(250)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(250)
+t.left(90)
+t.forward(100)
+
+window.mainloop()
---
+++ lab_14-2.py
@@ -0,0 +1,18 @@
+# Author: ATN 3/17/22
+
+import turtle
+
+window = turtle.Screen()
+window.setup(500, 500)
+window.screensize(450, 450)
+
+t = turtle.Turtle()
+
+t.right(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+
+window.mainloop()
