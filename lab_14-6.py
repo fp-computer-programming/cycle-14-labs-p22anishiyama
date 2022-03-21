40c9becce7575ec38824d25571c9cb596549b0dd
p22anishiyama <p22anishiyama@fairfieldprep.org>
2022-03-21 10:14:35 -0400

    revise lab_14-6.py

--- lab_14-6.py
+++ lab_14-6.py
@@ -7,14 +7,19 @@ window = turtle.Screen()
 
 # naming the turtle and giving its color and speed
 t = turtle.Turtle()
-t.color(window.textinput("Color", "Please choose a turtle color: "))
+color = window.textinput("Color", "Please choose a turtle color: ")
+t.color(color)
 t.shapesize(window.numinput("Size", "Please choose a turtle size (1-5): "))
 
 # creating the function for drawing a square
 def draw_square():
+    t.fillcolor(color)
+    t.begin_fill()
     for x in range(4):
         t.right(90)
         t.forward(100)
+    t.end_fill()
+
 
 # checking for user click
 window.onclick(draw_square(), btn=1)
